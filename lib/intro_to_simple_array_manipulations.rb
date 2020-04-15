acf2410a0f840401c4992c4b3103988cc3b43cc7
Matthew Malecki <mmalecki4@gmail.com>
2020-04-15 18:31:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete(array, string) 
-  array.delete(string)
+def using_delete_at(array, int) 
+  array.delete_at(int)
 end
